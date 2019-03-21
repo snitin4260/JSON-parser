facc201d2034c574c2ad5a6d1eb18b2368f43ceb
nitin <snitin8994@gmail.com>
2019-03-21 06:27:19 +0530

    Reimplement object parser

--- jsonParser.js
+++ jsonParser.js
@@ -3,6 +3,8 @@ const nullParser = input => (result = input.match(/^null/)) && [null, input.slic
 const trueParser = input => (result = input.match(/^true/)) && [true, input.slice(4)]
 const falseParser = input => (result = input.match(/^false/)) && [false, input.slice(5)]
 const numberParser = input => (result = input.match(/^-?(0|([1-9][0-9]*))(\.[0-9]+)?([E][+-]?[0-9]+)?/i)) && [result[0], input.slice(result[0].length)]
+const spaceParser = input => input.replace(/^\s+/, '')
+
 const stringParser = input => {
   if (input[0] !== '"') return null
   let str = ''; let i = 1; let isEscape = false
@@ -24,6 +26,7 @@ const stringParser = input => {
     i = i + 1
   }
 }
+
 const arrayParser = input => {
   if (input[0] !== '[') return null; else input = input.slice(1)
   let newArr = []
@@ -36,85 +39,54 @@ const arrayParser = input => {
       else {
         newArr.push(match[0])
         input = match[1]
-        // same like objects  when "    ]"  is
-        // present it will fail
-        // so do this here
+        // same like objects  when "    ]"  is present it will fail, so remove space
         input = input.replace(/^\s+/, '')
       }
     } else {
-      // remove ,
       input = input.slice(1).replace(/^\s+/, '')
       if (input[0] === ']') return null
-      // cannot have ,, in succession
       if (input[0] === ',') return null
     }
   }
-  if (input[0] === ']') return [newArr, input.slice(1)]
+  return [newArr, input.slice(1)]
 }
 
 const objectParser = input => {
   if (input[0] !== '{') return null
-  let newObj = {}; let currentKey; let valueFound = false; let keyShouldBeParsed = true; let colon = false
-  input = input.slice(1).replace(/^\s+/, '')
+  let newObj = {}; let match; let key
+  input = input.slice(1)
+  input = spaceParser(input)
+  if (input[0] === '}') return [newObj, input.slice(1)]
   while (input[0] !== '}') {
-    if (keyShouldBeParsed) {
-      let match = stringParser(input)
-      if (!match) return null
-      else {
-        currentKey = match[0]
-        input = match[1]
-        keyShouldBeParsed = false
-      }
-    } else {
-      // again remove whitespace
-      // until , or a value is encountered
-      input = input.replace(/^\s+/, '')
-      if (!colon) {
-        if (input[0] === ':') {
-          input = input.slice(1)
-          colon = true
-        } else return null
-      } else if (colon && !valueFound) {
-        // again check for white spaces
-        input = input.replace(/^\s+/, '')
-        let match = valueParser(input)
-        if (!match) {
-          return null
-        } else {
-          newObj[currentKey] = match[0]
-          input = match[1]
-          valueFound = true
-        }
-      } else if (valueFound) {
-        input = input.replace(/^\s+/, '')
-        // after value found if we have /n  } , check that case
-        if (input[0] === '}') break
-        if (input[0] !== ',') {
-          return null
-        } else {
-          // there can't be } after ,
-          input = input.slice(1)
-          // this is for cases where there is space after
-          //, like ,       }
-          input = input.replace(/^\s+/, '')
-          if (input[0] === '}') return null
-          colon = false
-          keyShouldBeParsed = true
-          valueFound = false
-        }
-      }
+    input = spaceParser(input)
+    match = stringParser(input)
+    if (!match) return null
+    else { key = match[0] }
+    input = match[1]
+    input = spaceParser(input)
+    if (input[0] !== ':') return null
+    input = input.slice(1)
+    input = spaceParser(input)
+    match = valueParser(input)
+    if (!match) return null
+    else {
+      newObj[key] = match[0]
+      console.log(newObj)
+      input = match[1]
+    }
+    input = spaceParser(input)
+    if (input[0] === '}') return [newObj, input.slice(1)]
+    else {
+      if (input[0] === ',') input = input.slice(1)
+      else return null
     }
   }
-  if (input[0] === '}') {
-    return [newObj, input.slice(1)]
-  }
+  return [newObj, input.slice(1)]
 }
-
-console.log(objectParser('{  }'))
 
 const valueParser = input => {
   input = input.replace(/^\s+/, '')
-  const parsers = [nullParser, trueParser, falseParser, numberParser, stringParser, arrayParser, objectParser]
+  const parsers = [stringParser, nullParser, trueParser, falseParser, numberParser, arrayParser, objectParser]
   let value
   for (let parser of parsers) {
     if (!value) {
@@ -130,8 +102,8 @@ const main = path => {
   fs.readFile(path, function read (err, data) {
     if (err) throw err
     content = data
-    let result = content.toString('utf8')
-    console.log(JSON.stringify(valueParser(result)))
+    console.log(JSON.stringify(valueParser(content.toString('utf8'))))
   })
 }
+
 main(`./test/pass1.json`)
